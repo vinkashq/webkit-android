5950fcb74f340018f11bc3c49d5319dd56fb53af
vinothkannans <vinothkannan@vinkas.com>
2016-05-22 03:51:02 +0530

    sample app created

--- module/src/main/java/com/vinkas/webkit/WebView.java
+++ module/src/main/java/com/vinkas/webkit/WebView.java
@@ -1,5 +1,6 @@
 package com.vinkas.webkit;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,6 +26,7 @@ public class WebView extends android.webkit.WebView {
         initialize();
     }
 
+    @TargetApi(21)
     public WebView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         initialize();
